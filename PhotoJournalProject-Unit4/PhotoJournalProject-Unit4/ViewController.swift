7ddba1d0f108beedf20055ca5373aa0fc52296be
karyRmartinez <karymartinez@pursuit.org>
2019-10-03 16:30:28 -0400

    test commit

---
+++ PhotoJournalProject-Unit4/PhotoJournalProject-Unit4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournalProject-Unit4
+//
+//  Created by Kary Martinez on 10/3/19.
+//  Copyright © 2019 Kary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

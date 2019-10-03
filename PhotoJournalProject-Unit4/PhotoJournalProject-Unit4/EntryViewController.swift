ef2ea1aff383a5e2fd8ab426e229f98c2e076583
karyRmartinez <karymartinez@pursuit.org>
2019-10-03 17:20:13 -0400

    imagePicker stuff

--- PhotoJournalProject-Unit4/PhotoJournalProject-Unit4/EntryViewController.swift
+++ PhotoJournalProject-Unit4/PhotoJournalProject-Unit4/EntryViewController.swift
@@ -12,7 +12,15 @@ class EntryViewController: UIViewController {
 
     @IBOutlet weak var EditingTextView: UITextView!
     
+    var photoLibraryAccess = false
+    
     @IBOutlet weak var EditingImage: UIImageView!
+    
+    var image = UIImage() {
+        didSet {
+            EditingImage.image = image
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +30,34 @@ class EntryViewController: UIViewController {
 
     @IBAction func photoLibraryAccess(_ sender: UIBarButtonItem) {
         
-        
-        
-    }
+        let imagePickerViewController = UIImagePickerController()
+              imagePickerViewController.delegate = self
+              imagePickerViewController.sourceType = .photoLibrary
+              
+              if photoLibraryAccess{
+                  imagePickerViewController.delegate = self
+                  present(imagePickerViewController, animated: true, completion: nil)
+              } else {
+                  let alertVC = UIAlertController(title: "No Access", message: "Camera access is required to use this app.", preferredStyle: .alert)
+                  alertVC.addAction(UIAlertAction (title: "Deny", style: .destructive, handler: nil))
+                  self.present(alertVC, animated: true, completion: nil)
+                  
+                  alertVC.addAction(UIAlertAction (title: "I will let you in", style: .default, handler: { (action) in
+                      self.photoLibraryAccess = true
+                      self.present(imagePickerViewController, animated: true, completion: nil)
+                  }))
+              }
+          }
+    
 }
 
+extension EntryViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else {
+            //couldn't get image :(
+            return
+        }
+        self.image = image
+        dismiss(animated: true, completion: nil)
+    }
+}

63027e2ff8e6948003891d88f29e498a8f2f081d
karyRmartinez <karymartinez@pursuit.org>
2019-10-04 17:42:47 -0400

    cancel button

---
+++ PhotoJournalProject-Unit4/PhotoJournalProject-Unit4/Photo.swift
@@ -0,0 +1,9 @@
+//
+//  Photo.swift
+//  PhotoJournalProject-Unit4
+//
+//  Created by Kary Martinez on 10/4/19.
+//  Copyright © 2019 Kary Martinez. All rights reserved.
+//
+
+import Foundation
---
+++ PhotoJournalProject-Unit4/PhotoJournalProject-Unit4/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoJournalProject-Unit4
+//
+//  Created by Kary Martinez on 10/4/19.
+//  Copyright © 2019 Kary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
